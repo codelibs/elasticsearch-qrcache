3a9924a6254fc4755461a3338baa6eb0f1e9c90b
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2015-03-26 22:40:06 +0900

    fix #7

--- src/main/java/org/codelibs/elasticsearch/qrcache/filter/RefreshEngineFilter.java
+++ src/main/java/org/codelibs/elasticsearch/qrcache/filter/RefreshEngineFilter.java
@@ -4,7 +4,6 @@ import org.codelibs.elasticsearch.extension.chain.EngineChain;
 import org.codelibs.elasticsearch.extension.filter.BaseEngineFilter;
 import org.codelibs.elasticsearch.qrcache.cache.QueryResultCache;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.index.engine.Engine.Refresh;
 import org.elasticsearch.index.engine.EngineException;
 
 public class RefreshEngineFilter extends BaseEngineFilter {
@@ -22,10 +21,10 @@ public class RefreshEngineFilter extends BaseEngineFilter {
     }
 
     @Override
-    public void doRefresh(final Refresh refresh, final EngineChain chain)
+    public void doRefresh(final String source, final EngineChain chain)
             throws EngineException {
-        chain.doRefresh(refresh);
-        queryResultCache.clear(chain.getEngine().shardId().getIndex());
+        chain.doRefresh(source);
+        queryResultCache.clear(chain.getEngineConfig().getShardId().getIndex());
     }
 
 }
